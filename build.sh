b5e23daef9018e3a030b048c52f099bb2e9a2207
yoavsreb <yoav.srebrnik@gmail.com>
2016-12-19 22:18:48 -0800

    update script to be more general

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 rm -rf target/*
-export GOOGLETEST_LIBS_DIR=/home/yoav/CppProjects/googletest/build/googletest
-export GOOGLETEST_INCLUDE_DIR=/home/yoav/CppProjects/googletest/googletest/include
-export BOOST_DIR=/home/yoav/opt/boost_1_61_0
+export GOOGLETEST_LIBS_DIR=`pwd`/../googletest/build/googletest
+export GOOGLETEST_INCLUDE_DIR=`pwd`/../googletest/googletest/include
+export BOOST_DIR=~/opt/boost_1_61_0
 cmake . -Btarget
 cd target
 make VERBOSE=1
