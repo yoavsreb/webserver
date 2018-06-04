6f5bf81eaacd9cfe97eae8b780093759201b5ee5
Yoav Srebrnik <yoav.srebrnik@gmail.com>
2018-06-03 22:23:18 -0700

    cleanup build a little bit, add more beef to test

--- query/test/querytests1.cpp
+++ query/test/querytests1.cpp
@@ -10,5 +10,9 @@ TEST(QueryTests, QueryTest1) {
 TEST(QueryTests, QueryTest2) {
     auto ptr = parseString("{Node v1 | v1[v2]}");
     EXPECT_TRUE(ptr.get() != nullptr);
+    EXPECT_TRUE(ptr->pExpression->type == ExpressionType::REF_ATTR_EXPR);
+    EXPECT_TRUE(ptr->pDeclarations->type == ExpressionType::DECLS);
+    EXPECT_TRUE(ptr->pDeclarations->subExpr.size() == 1);
+    EXPECT_TRUE(ptr->pDeclarations->subExpr[0]->type == ExpressionType::DECL_NODE);
     ptr.reset();
 }
